5f589f1742f5acdc7d9a350f898c3639054479cb
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-06-24 23:59:38 +0000

    Menu Tree para Notas

--- trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/mb/MenuBackingBean.java
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/mb/MenuBackingBean.java
@@ -13,6 +13,8 @@ import javax.faces.component.UIComponent;
 
 import org.primefaces.component.accordionpanel.AccordionPanel;
 import org.primefaces.event.TabChangeEvent;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 import cl.mdr.ifrs.ejb.cross.service.local.TestServiceLocal;
 
@@ -34,6 +36,30 @@ public class MenuBackingBean extends AbstractBackingBean implements Serializable
 	private AccordionPanel menuAcordionPanel;
 	private String activeTabIndex;
 	
+	private TreeNode root;
+	
+	
+	
+	@SuppressWarnings("unused")
+	public MenuBackingBean() {
+		super();
+		root = new DefaultTreeNode("Root", null);  
+		TreeNode node0 = new DefaultTreeNode("Node 0", root);  
+        TreeNode node1 = new DefaultTreeNode("Node 1", root);  
+        TreeNode node2 = new DefaultTreeNode("Node 2", root);  
+  
+        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);  
+        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);  
+  
+        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);  
+        TreeNode node11 = new DefaultTreeNode("Node 1.1", node1);  
+  
+        TreeNode node000 = new DefaultTreeNode("Node 0.0.0", node00);  
+        TreeNode node001 = new DefaultTreeNode("Node 0.0.1", node00);  
+        TreeNode node010 = new DefaultTreeNode("Node 0.1.0", node01);  
+  
+        TreeNode node100 = new DefaultTreeNode("Node 1.0.0", node10); 
+	}
 	
 	public void onTabChange(TabChangeEvent event) {  
 		this.setTabActivo(event.getTab());   
@@ -68,6 +94,14 @@ public class MenuBackingBean extends AbstractBackingBean implements Serializable
 
 	public void setActiveTabIndex(String activeTabIndex) {
 		this.activeTabIndex = activeTabIndex;
+	}
+
+	public TreeNode getRoot() {
+		return root;
+	}
+
+	public void setRoot(TreeNode root) {
+		this.root = root;
 	}
 
 	 
