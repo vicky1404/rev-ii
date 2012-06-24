53b62242509271eb620358a8e6789e81aae00f64
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-06-24 23:38:27 +0000

    -implementacion de Layout de Aplicacion. 	-crecion de facelets template para la aplicacion. 	-template de menu tipo acordeon. 	-abstract backing bean.

---
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/mb/AbstractBackingBean.java
@@ -0,0 +1,43 @@
+package cl.mdr.ifrs.cross.mb;
+
+import java.security.Principal;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+public abstract class AbstractBackingBean {
+
+	public FacesContext getFacesContext(){
+		return FacesContext.getCurrentInstance();
+	}
+	
+	public ExternalContext getExternalContext(){
+		return this.getFacesContext().getExternalContext();
+	}
+
+	public Principal getPrincipal() {
+		return this.getExternalContext().getUserPrincipal();
+	}
+
+	public void addErrorMessage(String summary, String detail) {
+		addMessage(FacesMessage.SEVERITY_ERROR, summary, detail);
+	}
+
+	public void addFatalMessage(String summary, String detail) {
+		addMessage(FacesMessage.SEVERITY_FATAL, summary, detail);
+	}
+
+	public void addInfoMessage(String summary, String detail) {
+		addMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+	}
+
+	public void addWarnMessage(String summary, String detail) {
+		addMessage(FacesMessage.SEVERITY_WARN, summary, detail);
+	}
+
+	private void addMessage(Severity severity, String summary, String detail) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(severity, summary, detail));
+	}
+}
