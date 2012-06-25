4e1cd8bd5b3476b3a5ba3fde9bb6935de22bdf55
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-06-25 00:20:39 +0000

    Menu Tree para Notas

--- trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/mb/MenuBackingBean.java
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/mb/MenuBackingBean.java
@@ -43,22 +43,16 @@ public class MenuBackingBean extends AbstractBackingBean implements Serializable
 	@SuppressWarnings("unused")
 	public MenuBackingBean() {
 		super();
-		root = new DefaultTreeNode("Root", null);  
-		TreeNode node0 = new DefaultTreeNode("Node 0", root);  
-        TreeNode node1 = new DefaultTreeNode("Node 1", root);  
-        TreeNode node2 = new DefaultTreeNode("Node 2", root);  
+		root = new DefaultTreeNode("Proceso", null);  
+		TreeNode node0 = new DefaultTreeNode("Notas", root);  
+        TreeNode node1 = new DefaultTreeNode("Cuadros Tecnicos", root);   
   
-        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);  
-        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);  
+        TreeNode node00 = new DefaultTreeNode("Nota 1", node0);  
+        TreeNode node01 = new DefaultTreeNode("Nota 2", node0);  
   
-        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);  
-        TreeNode node11 = new DefaultTreeNode("Node 1.1", node1);  
-  
-        TreeNode node000 = new DefaultTreeNode("Node 0.0.0", node00);  
-        TreeNode node001 = new DefaultTreeNode("Node 0.0.1", node00);  
-        TreeNode node010 = new DefaultTreeNode("Node 0.1.0", node01);  
-  
-        TreeNode node100 = new DefaultTreeNode("Node 1.0.0", node10); 
+        TreeNode node10 = new DefaultTreeNode("Cuadro 6.1", node1);  
+        TreeNode node11 = new DefaultTreeNode("Cuadro 6.2", node1);  
+
 	}
 	
 	public void onTabChange(TabChangeEvent event) {  
