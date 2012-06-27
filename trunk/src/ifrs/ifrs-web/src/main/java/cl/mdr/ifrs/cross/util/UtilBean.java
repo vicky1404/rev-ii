84addd1a85e8ca910c786a3b7e00985268c525a6
rodivasq@gmail.com <rodivasq@gmail.com@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-06-27 03:06:35 +0000

    se agregan clases de apoyo

---
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/util/UtilBean.java
@@ -0,0 +1,28 @@
+package cl.mdr.ifrs.cross.util;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+
+public class UtilBean {
+	
+    @SuppressWarnings("unchecked")
+    public static <T> T findBean(String name) {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        return (T)ctx.getApplication().evaluateExpressionGet(ctx, "#{" + name + "}", Object.class);      
+    }
+    
+    public static void sortSelectItemMeses(List<SelectItem> meses){
+        Collections.sort(meses, new Comparator<SelectItem>(){                        
+            public int compare(SelectItem s1, SelectItem s2){ 
+                Integer i1 = Integer.parseInt(s1.getLabel());
+                Integer i2 = Integer.parseInt(s2.getLabel());
+                return i1.compareTo(i2);
+            }
+        });
+    }
+
+}
