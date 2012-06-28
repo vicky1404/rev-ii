b71fdc00c2e9b12cc526a1e7ef94429f200cba8f
rodivasq@gmail.com <rodivasq@gmail.com@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-06-28 02:19:23 +0000

    Generador de version

--- trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/modules/mb/GeneradorVersionBackingBean.java
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/modules/mb/GeneradorVersionBackingBean.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 
 import cl.mdr.ifrs.cross.mb.AbstractBackingBean;
 import cl.mdr.ifrs.ejb.entity.Catalogo;
 import cl.mdr.ifrs.ejb.entity.TipoCuadro;
+import cl.mdr.ifrs.ejb.entity.Version;
 
 
 @ManagedBean
@@ -17,13 +20,44 @@ public class GeneradorVersionBackingBean extends AbstractBackingBean{
 	
 	private TipoCuadro tipoCuadro;
 	private Catalogo catalogo;
+	private Long idCatalogo;
 	private List<Catalogo> catalogoList;
+	private List<Version> versionList;
 	
-	
-	public List<Catalogo> completeCatalogo(String query) {  
-        List<Catalogo> suggestions = new ArrayList<Catalogo>();  
-          
-        for(Catalogo p : getCatalogoList()) {  
+	public void buscarListener(ActionEvent event){
+		
+		System.out.println(idCatalogo);
+		
+		try{
+			catalogo = getFacadeService().getCatalogoService().findCatalogoByCatalogo(new Catalogo(idCatalogo));
+			
+			if(catalogo!=null)
+				versionList = getFacadeService().getVersionService().findVersionAllByCatalogo(catalogo);
+			else
+				addInfoMessage("Sin versiones", "No existen versiones para catalogo seleccionado");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	public Long getIdCatalogo() {
+		return idCatalogo;
+	}
+
+	public void setIdCatalogo(Long idCatalogo) {
+		this.idCatalogo = idCatalogo;
+	}
+
+	public List<Catalogo> completeCatalogo(String query) {
+		
+		List<Catalogo> suggestions = new ArrayList<Catalogo>();  
+		
+		if(catalogoList==null)
+			return suggestions;
+		
+        
+        for(Catalogo p : catalogoList) {  
         	if(p.getNombre().toUpperCase().indexOf(query.toUpperCase()) >= 0)
                 suggestions.add(p);  
         }  
@@ -45,14 +79,30 @@ public class GeneradorVersionBackingBean extends AbstractBackingBean{
 	}
 
 	public List<Catalogo> getCatalogoList() {
-		if(catalogoList==null){
-			catalogoList = getFacadeService().getCatalogoService().findCatalogoVigenteAll();
-		}
 		return catalogoList;
+	}
+	
+	public void tipoCatalogoChangeListener(){
+		try{
+			if(tipoCuadro!=null)
+				catalogoList = getFacadeService().getCatalogoService().findCatalogoByFiltro(getNombreUsuario(), tipoCuadro, null, 1L);
+		}catch(Exception e){
+			addErrorMessage("Error al buscar catalogo", "Error al buscar catalogo");
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void setCatalogoList(List<Catalogo> catalogoList) {
 		this.catalogoList = catalogoList;
+	}
+
+	public List<Version> getVersionList() {
+		return versionList;
+	}
+
+	public void setVersionList(List<Version> versionList) {
+		this.versionList = versionList;
 	}
 	
 
