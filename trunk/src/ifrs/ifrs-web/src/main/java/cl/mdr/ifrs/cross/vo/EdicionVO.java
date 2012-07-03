02796b246b15f68d14ed7356ccccb609ddfb233c
rodivasq@gmail.com <rodivasq@gmail.com@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-07-03 23:15:42 +0000

    Se actualizan cambios para diseñador

---
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/vo/EdicionVO.java
@@ -0,0 +1,43 @@
+package cl.mdr.ifrs.cross.vo;
+
+public class EdicionVO {
+    
+    private String titulo;
+    private Object valor;
+    private Long ancho;
+    
+    public EdicionVO() {
+        super();
+    }
+
+    public EdicionVO(String titulo, Object valor, Long ancho) {
+        super();
+        this.titulo = titulo;
+        this.valor = valor;
+        this.ancho = ancho;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
+
+    public Object getValor() {
+        return valor;
+    }
+
+    public void setAncho(Long ancho) {
+        this.ancho = ancho;
+    }
+
+    public Long getAncho() {
+        return ancho;
+    }
+}
