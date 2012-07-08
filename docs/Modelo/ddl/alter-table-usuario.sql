5e4316135d109b6c7370fb631b83930c28fe8f9d
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-07-08 23:01:14 +0000

    Modificaciones para la administracion de usuario

---
+++ docs/Modelo/ddl/alter-table-usuario.sql
@@ -0,0 +1,10 @@
+alter table IFRS_USUARIO
+add NOMBRE  varchar2(256 CHAR) NULL; 
+
+alter table IFRS_USUARIO
+add APELLIDO_PATERNO  varchar2(256 CHAR) NULL; 
+
+alter table IFRS_USUARIO
+add APELLIDO_MATERNO  varchar2(256 CHAR) NULL;       
+
+	  
