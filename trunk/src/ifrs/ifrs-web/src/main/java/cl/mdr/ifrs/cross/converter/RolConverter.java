728de7af3a14aea94992d8c57bbca3f0760bc31f
rodivasq@gmail.com <rodivasq@gmail.com@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-07-09 13:35:04 +0000

    se agrega anotacion

--- trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/converter/RolConverter.java
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/cross/converter/RolConverter.java
@@ -3,6 +3,7 @@ package cl.mdr.ifrs.cross.converter;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 
 import org.apache.log4j.Logger;
 
@@ -12,6 +13,7 @@ import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+@FacesConverter(value="rolConverter" ,forClass=Rol.class)
 public class RolConverter implements Converter {
 	
 	private static final Logger LOGGER = Logger.getLogger(RolConverter.class);
