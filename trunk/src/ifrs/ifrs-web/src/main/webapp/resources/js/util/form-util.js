9be276f1dbfaec0e011bd0dd41a08f08eb0439b0
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-07-12 22:43:22 +0000

    Editor html en proceso

--- trunk/src/ifrs/ifrs-web/src/main/webapp/resources/js/util/form-util.js
+++ trunk/src/ifrs/ifrs-web/src/main/webapp/resources/js/util/form-util.js
@@ -9,3 +9,12 @@ function unSelectAll(){
         this.checked = false;                        
     });   
 }
+
+function getWidth(){ 
+	return $('#l_center').width();
+}
+
+function setEditorWidth(){
+	$('.ui-editor').css("width", "100%");
+	$('.ui-editor').find('iframe').css("width", "100%");
+}
