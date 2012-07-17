9c6676603f4494721d0c03d9cead6dba707a53f6
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-07-17 00:44:54 +0000

    Configurador de Cuadros

--- src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/modules/configuracion/mb/ConfiguradorDisenoBackingBean.java
+++ src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/modules/configuracion/mb/ConfiguradorDisenoBackingBean.java
@@ -1,9 +1,13 @@
 package cl.mdr.ifrs.modules.configuracion.mb;
 
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 
 import org.apache.log4j.Logger;
 import org.primefaces.event.FileUploadEvent;
@@ -11,7 +15,11 @@ import org.primefaces.model.UploadedFile;
 
 import cl.mdr.ifrs.cross.mb.AbstractBackingBean;
 import cl.mdr.ifrs.cross.util.GeneradorDisenoHelper;
+import cl.mdr.ifrs.ejb.entity.Celda;
+import cl.mdr.ifrs.ejb.entity.Columna;
 import cl.mdr.ifrs.ejb.entity.Grilla;
+import cl.mdr.ifrs.ejb.entity.TipoCelda;
+import cl.mdr.ifrs.ejb.entity.TipoDato;
 import cl.mdr.ifrs.exceptions.CargaGrillaExcelException;
 import cl.mdr.ifrs.vo.GrillaVO;
 
@@ -27,13 +35,19 @@ public class ConfiguradorDisenoBackingBean extends AbstractBackingBean implement
 	private static final long serialVersionUID = -1303743504274782576L;
 	public static final String BEAN_NAME = "configuradorDisenoBackingBean"; 
 	
-	/*atributos utilizados para la upload de archivo*/
-    private transient UploadedFile uploadedFile;
+	
     private GrillaVO grillaVO;
+    private Grilla grilla;
+    private Long idTipoCelda;
+    private Long idTipoDato;
+    
+    /*atributos utilizados para la upload de archivo*/
+    private transient UploadedFile uploadedFile;
     
     
-	/**
-	 * Carga mediante Excel
+    /**
+	 * Carga mediante Libro 
+	 * Excel
 	 * 
 	 **/
 
@@ -51,10 +65,10 @@ public class ConfiguradorDisenoBackingBean extends AbstractBackingBean implement
             if(this.getUploadedFile().getInputstream()  == null){
             	super.addErrorMessage("Seleccione o Actualice el archivo que desea cargar");
                 return;
-            }
-            final Grilla grilla = super.getFacadeService().getCargadorEstructuraService().getGrillaByExcel(this.getUploadedFile().getInputstream()); 
-            this.setGrillaVO(super.getFacadeService().getEstructuraService().getGrillaVO(grilla, Boolean.FALSE));
-            this.getGrillaVO().setCeldaList(GeneradorDisenoHelper.builHtmlGrilla(grilla.getColumnaList()));
+            }            
+            this.setGrilla(super.getFacadeService().getCargadorEstructuraService().getGrillaByExcel(this.getUploadedFile().getInputstream()));
+            this.setGrillaVO(super.getFacadeService().getEstructuraService().getGrillaVO(this.getGrilla(), Boolean.FALSE));
+            this.getGrillaVO().setCeldaList(GeneradorDisenoHelper.builHtmlGrilla(this.getGrilla().getColumnaList()));
 
         } catch (CargaGrillaExcelException e) {
             logger.error("error al procesar archivo excel ",e);
@@ -67,6 +81,42 @@ public class ConfiguradorDisenoBackingBean extends AbstractBackingBean implement
        
     }
     
+    /**
+     * @param event
+     */
+    public void editarColumnaAction(ActionEvent event){    	
+    	Columna columna = (Columna) event.getComponent().getAttributes().get("columna");
+    	logger.info("editando columna "+columna.getTituloColumna());
+    	logger.info("tipo celda "+this.getIdTipoCelda());
+    	logger.info("tipo dato "+this.getIdTipoDato());
+    	Iterator<List<Celda>> iter = this.getGrillaVO().getCeldaList().iterator();
+    	while(iter.hasNext()){
+    	    Iterator<Celda> celdaIter = iter.next().iterator();
+    	    while(celdaIter.hasNext()){
+    	    	 Celda celda = celdaIter.next();
+    	    	 if(celda.getIdColumna().equals(columna.getIdColumna())){
+    	    		 celda.setTipoCelda(new TipoCelda(this.getIdTipoCelda()));
+    	    		 celda.setTipoDato(new TipoDato(this.getIdTipoDato()));    		 
+    	    	 }    	         
+    	     }
+    	}
+    }
+    
+    public void onChangeTipoCeldaListener(ValueChangeEvent valueChangeEvent){
+    	if(valueChangeEvent.getNewValue() != null){
+    		this.setIdTipoCelda(null);    	
+    		this.setIdTipoCelda((Long) valueChangeEvent.getNewValue());
+    		logger.info("change tipo celda "+this.getIdTipoCelda());
+    	}
+    }
+    
+    public void onChangeTipoDatoListener(ValueChangeEvent valueChangeEvent){
+    	if(valueChangeEvent.getNewValue() != null){
+    		this.setIdTipoDato(null);	
+    		this.setIdTipoDato((Long) valueChangeEvent.getNewValue());    		
+        	logger.info("change tipo dato "+this.getIdTipoDato());
+    	}
+    }
  
 	public UploadedFile getUploadedFile() {
 		return uploadedFile;
@@ -85,6 +135,30 @@ public class ConfiguradorDisenoBackingBean extends AbstractBackingBean implement
 
 	public void setGrillaVO(GrillaVO grillaVO) {
 		this.grillaVO = grillaVO;
+	}
+	
+	public Grilla getGrilla() {
+		return grilla;
+	}
+
+	public void setGrilla(Grilla grilla) {
+		this.grilla = grilla;
+	}
+
+	public Long getIdTipoCelda() {
+		return idTipoCelda;
+	}
+
+	public void setIdTipoCelda(Long idTipoCelda) {
+		this.idTipoCelda = idTipoCelda;
+	}
+
+	public Long getIdTipoDato() {
+		return idTipoDato;
+	}
+
+	public void setIdTipoDato(Long idTipoDato) {
+		this.idTipoDato = idTipoDato;
 	}
 
 }
