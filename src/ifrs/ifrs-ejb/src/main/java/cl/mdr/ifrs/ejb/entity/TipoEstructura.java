1cef7c07103556f0f79c374f5f3a9a7480d97f22
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-07-20 20:07:42 +0000

    Configurador de Estructuras

--- src/ifrs/ifrs-ejb/src/main/java/cl/mdr/ifrs/ejb/entity/TipoEstructura.java
+++ src/ifrs/ifrs-ejb/src/main/java/cl/mdr/ifrs/ejb/entity/TipoEstructura.java
@@ -2,15 +2,12 @@ package cl.mdr.ifrs.ejb.entity;
 
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import cl.mdr.ifrs.ejb.common.Constantes;
