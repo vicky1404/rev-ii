6ae81c9c325dcb6ac46dfa1ff7f87418cf9c4fc7
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-08-11 00:54:24 +0000

    Initial import. API Xbrl Core

---
+++ src/xbrl-core/src/xbrlcore/junit/AllTests.java
@@ -0,0 +1,44 @@
+package xbrlcore.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import xbrlcore.junit.base.ConceptTest;
+import xbrlcore.junit.base.DTSExtendedLinkbaseTests;
+import xbrlcore.junit.base.DTSFactoryTest;
+import xbrlcore.junit.base.InvalidTaxonomyTest;
+import xbrlcore.junit.dimensions.DimensionTest;
+import xbrlcore.junit.dimensions.MultipleDimensionTypeTest;
+import xbrlcore.junit.instance.FactTest;
+import xbrlcore.junit.instance.InstanceTestTmp;
+import xbrlcore.junit.instance.XBRLInstanceFactoryTest;
+import xbrlcore.junit.instance.XBRLInstanceTest;
+import xbrlcore.junit.linkbase.CalculationLinkbaseTest;
+import xbrlcore.junit.linkbase.DefinitionLinkbaseTest;
+import xbrlcore.junit.linkbase.LabelLinkbaseTest;
+import xbrlcore.junit.linkbase.PresentationLinkbaseTest;
+import xbrlcore.junit.linkbase.XBRLLinkbaseTest;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+		DTSFactoryTest.class, 
+		ConceptTest.class,
+		XBRLLinkbaseTest.class, 
+		LabelLinkbaseTest.class,
+		PresentationLinkbaseTest.class, 
+		DefinitionLinkbaseTest.class,
+		CalculationLinkbaseTest.class, 
+		DTSExtendedLinkbaseTests.class,
+		InvalidTaxonomyTest.class, 
+		DimensionTest.class, 
+		FactTest.class,
+		XBRLInstanceTest.class, 
+		MultipleDimensionTypeTest.class,
+		XBRLInstanceFactoryTest.class, 
+		InstanceTestTmp.class })
+public final class AllTests {
+    
+    private AllTests() {
+    }
+}
