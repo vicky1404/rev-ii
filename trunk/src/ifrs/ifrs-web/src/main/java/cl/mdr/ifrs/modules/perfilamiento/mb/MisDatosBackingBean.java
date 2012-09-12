ecee9f4ebad9477a9137d5763e404762c15dd6f2
rodrigo.reyesco <rodrigo.reyesco@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-09-12 20:41:16 +0000

    funcionalidad de cambio de clave inicial

--- trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/modules/perfilamiento/mb/MisDatosBackingBean.java
+++ trunk/src/ifrs/ifrs-web/src/main/java/cl/mdr/ifrs/modules/perfilamiento/mb/MisDatosBackingBean.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.apache.log4j.Logger;
 
 import cl.mdr.ifrs.cross.mb.AbstractBackingBean;
+import cl.mdr.ifrs.cross.mb.PropertyManager;
 import cl.mdr.ifrs.ejb.entity.Usuario;
 
 @ManagedBean
@@ -22,6 +24,23 @@ public class MisDatosBackingBean extends AbstractBackingBean implements Serializ
 	@PostConstruct
 	void init(){
 		this.setUsuario(super.getUsuarioSesion());		
+	}
+	
+	public void editAction(ActionEvent actionEvent){
+		try {
+			Usuario usuario = this.getUsuario();
+			final Usuario usuarioPersisted = super.getFacadeService().getSeguridadService().authenticateUser(usuario.getNombreUsuario());
+			if(usuario.getCambiarPassword().equals(1L) && usuario.getPassword().equals(usuarioPersisted.getPassword())){
+				super.addWarnMessage("La nueva clave no debe ser igual a la generda por el Administrador del Sistema");
+				return;
+			}					
+			usuario.setCambiarPassword(0L);
+			this.getFacadeService().getSeguridadService().mergeUsuario(usuario);
+			super.addInfoMessage("Estimado Usuario, sus datos se han actualizado correctamente.");
+		} catch (Exception e) {
+			logger.error(e);
+			super.addErrorMessage(PropertyManager.getInstance().getMessage("mensaje_error_generico"));
+		}
 	}
 	
 	
