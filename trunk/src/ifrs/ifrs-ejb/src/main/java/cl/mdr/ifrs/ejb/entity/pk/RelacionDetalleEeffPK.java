035d5427bdb4e70d7b56c3a1c2fa51fb1449a8d5
rodivasq@gmail.com <rodivasq@gmail.com@17bb37fe-d5a1-dcfe-262a-f88f89c0b580>
2012-12-31 17:11:29 +0000

    Se corrigen bugs

--- trunk/src/ifrs/ifrs-ejb/src/main/java/cl/mdr/ifrs/ejb/entity/pk/RelacionDetalleEeffPK.java
+++ trunk/src/ifrs/ifrs-ejb/src/main/java/cl/mdr/ifrs/ejb/entity/pk/RelacionDetalleEeffPK.java
@@ -9,74 +9,71 @@ public class RelacionDetalleEeffPK implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 3260624593290924201L;
+	private Long idCuenta;
 	private Long idFecu;
-    private Long idPeriodo;
-    private Long idCuenta;
+	private Long idPeriodo;
+	private Long idRut;
+	private Long idGrilla;
+	private Long idColumna;
+	private Long idFila;
+	
+	public RelacionDetalleEeffPK(){
+	}
+	
+	public RelacionDetalleEeffPK(Long idCuenta, Long idFecu, Long idPeriodo,
+			Long idRut, Long idGrilla, Long idColumna, Long idFila) {
+		super();
+		this.idCuenta = idCuenta;
+		this.idFecu = idFecu;
+		this.idPeriodo = idPeriodo;
+		this.idRut = idRut;
+		this.idGrilla = idGrilla;
+		this.idColumna = idColumna;
+		this.idFila = idFila;
+	}
+	public Long getIdCuenta() {
+		return idCuenta;
+	}
+	public void setIdCuenta(Long idCuenta) {
+		this.idCuenta = idCuenta;
+	}
+	public Long getIdFecu() {
+		return idFecu;
+	}
+	public void setIdFecu(Long idFecu) {
+		this.idFecu = idFecu;
+	}
+	public Long getIdPeriodo() {
+		return idPeriodo;
+	}
+	public void setIdPeriodo(Long idPeriodo) {
+		this.idPeriodo = idPeriodo;
+	}
+	public Long getIdRut() {
+		return idRut;
+	}
+	public void setIdRut(Long idRut) {
+		this.idRut = idRut;
+	}
+	public Long getIdGrilla() {
+		return idGrilla;
+	}
+	public void setIdGrilla(Long idGrilla) {
+		this.idGrilla = idGrilla;
+	}
+	public Long getIdColumna() {
+		return idColumna;
+	}
+	public void setIdColumna(Long idColumna) {
+		this.idColumna = idColumna;
+	}
+	public Long getIdFila() {
+		return idFila;
+	}
+	public void setIdFila(Long idFila) {
+		this.idFila = idFila;
+	}
 
-    public RelacionDetalleEeffPK() {
-    }
-
-    public RelacionDetalleEeffPK(Long idFecu, Long idPeriodo, Long idCuenta) {
-        this.idFecu = idFecu;
-        this.idPeriodo = idPeriodo;
-        this.idCuenta = idCuenta;
-    }
-
-
-    public Long getIdFecu() {
-        return idFecu;
-    }
-
-    public void setIdFecu(Long idFecu) {
-        this.idFecu = idFecu;
-    }
-
-    public Long getIdPeriodo() {
-        return idPeriodo;
-    }
-
-    public void setIdPeriodo(Long idPeriodo) {
-        this.idPeriodo = idPeriodo;
-    }
-
-    public void setIdCuenta(Long idCuenta) {
-        this.idCuenta = idCuenta;
-    }
-
-    public Long getIdCuenta() {
-        return idCuenta;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (!(object instanceof RelacionDetalleEeffPK)) {
-            return false;
-        }
-        final RelacionDetalleEeffPK other = (RelacionDetalleEeffPK) object;
-        if (!(idFecu == null ? other.idFecu == null : idFecu.equals(other.idFecu))) {
-            return false;
-        }
-        if (!(idPeriodo == null ? other.idPeriodo == null : idPeriodo.equals(other.idPeriodo))) {
-            return false;
-        }
-        if (!(idCuenta == null ? other.idCuenta == null : idCuenta.equals(other.idCuenta))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        final int PRIME = 37;
-        int result = 1;
-        result = PRIME * result + ((idFecu == null) ? 0 : idFecu.hashCode());
-        result = PRIME * result + ((idPeriodo == null) ? 0 : idPeriodo.hashCode());
-        result = PRIME * result + ((idCuenta == null) ? 0 : idCuenta.hashCode());
-        return result;
-    }
 
 	   
 }
